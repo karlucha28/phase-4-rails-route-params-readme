d855d140db76774032ee43cd0a8cb19115ac7122
KarlSavinskiy <karlsavinskiy@gmail.com>
2022-09-22 12:56:07 -0400

    commit tree 68cc0d14f9c2eb0ecf6fba4fdbbe2166b9232ebd

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,12 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def show
+    #find a cheese using the ID from the URL
+    cheese = Cheese.find_by(id: params[:id])
+    # byebug
+    # send a JSON response using that cheese object
+    render json: cheese
+  end
+
 end
